9a53eae833252b085bfc19fc3d649da03bcb512f
omsi96 <omsi96@gmail.com>
2021-09-26 15:31:01 +0300

    html css js

---
+++ index.js
@@ -0,0 +1,43 @@
+let consoleText = "0";
+let operation = null;
+
+function buttonClick(text) {
+  console.log("Clicking", text);
+  if (text >= 0 && text <= 9) {
+    // number
+    if (operation !== null) {
+      // There was a previous operation
+      let consoleNumber = parseInt(consoleText);
+    } else {
+      // No previous operation
+      if (parseInt(consoleText) === 0) {
+        console.log("Text is 0");
+        consoleText = `${text}`;
+        printOnConsole(consoleText);
+      } else {
+        console.log(consoleText);
+        consoleText += text;
+        printOnConsole(consoleText);
+      }
+    }
+  } else {
+    // operation
+    switch (text) {
+      case "+":
+        break;
+      case "-":
+        break;
+      case "*":
+        break;
+      case "/":
+        break;
+      case "=":
+        break;
+    }
+  }
+}
+
+function printOnConsole(text) {
+  console.log(text);
+  document.getElementById("console").innerText = `${text}`;
+}
